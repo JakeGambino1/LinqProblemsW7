a60de78f2f4ad41ce741c420e4b17d90f993afd4
Jake Gambino <jake.gambino1@gmail.com>
2019-09-24 09:16:09 -0500

    return strings with 'th' method completed

---
+++ LinqProblems/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using LINQ, write function that returns all words containing "th"
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            // Using LINQ, write a function that takes in a list of strings and returns copy without duplicates
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            // write a function that:
+            // drops lowest grade
+            // averages the list
+            // averages those averages
+            // expected output = 86.125
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+
+            var wordsWithTh = words.Where(w => w.Contains("th"));
+            foreach(var word in wordsWithTh)
+            {
+                Console.WriteLine(word);
+            }
+                Console.ReadLine();
+
+            var namesNoDuplicates = names.Where(n => n.Distinct());
+
+
+
+        }
+    }
+}
